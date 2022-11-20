22efb1c17180cf9a269a7521cd417344db07fc8e
Bernard-Jelinic <jelinic.bernard@gmail.com>
2022-11-20 11:07:10 +0100

    Add home router

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+const routes = [
+    { path:'', component:home },
+]
+
+const router = new VueRouter({
+    routes
+})
+
 const app = new Vue({
+    router,
     components: { Navbar }
 }).$mount('#app')
---
+++ js/home.js
@@ -0,0 +1,5 @@
+const home = {
+    template: `
+        <p>Hello World!!!</p>
+    `
+}
