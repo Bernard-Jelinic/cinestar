2d6aa4fcc260648b4d9bde3dd4fed9f627811738
Bernard-Jelinic <jelinic.bernard@gmail.com>
2022-11-20 11:04:35 +0100

    Create Navbar component

---
+++ js/app.js
@@ -0,0 +1,3 @@
+const app = new Vue({
+    components: { Navbar }
+}).$mount('#app')
---
+++ js/navbar.js
@@ -0,0 +1,41 @@
+const Navbar = {
+    template: `
+      <nav class="header">
+        <div class="zvijezda">
+            <img src="img/cinestar-logotip.png" alt="CineStar" height="105" />
+        </div>
+        <div class="najbolji">
+          <a
+            href="https://www.youtube.com/watch?v=TUqa4fhc4us"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="img/najbolji-kinoprikazivac.png"
+              alt="Najbolji kinoprikazivač"
+            />
+          </a>
+        </div>
+        <div class="social">
+          <a
+            class="fab fa-facebook"
+            href="https://www.facebook.com/CineStarCinemasHrvatska"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          <a
+            class="fab fa-instagram"
+            href="https://instagram.com/cinestarcinemas_hr"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          <a
+            class="fab fa-youtube"
+            href="http://www.youtube.com/user/CineStarMultiplexi"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+        </div>
+      </nav>
+    `
+}
