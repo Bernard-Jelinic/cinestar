49d98bccdf8f22b18bc1310b46ca4ea9b732ea32
Bernard-Jelinic <jelinic.bernard@gmail.com>
2023-02-01 21:10:26 +0100

    Create cities object and display it in dropdown

---
+++ datas/Cities.js
@@ -0,0 +1,23 @@
+const dataCity = [
+	{
+		id: 1, name: "Cinestar Zagreb"
+	},
+	{
+		id: 2, name: "Cinestar Split"
+	},
+	{
+		id: 3, name: "Cinestar Rijeka"
+	},
+	{
+		id: 4, name: "Cinestar Osijek"
+	},
+	{
+		id: 5, name: "Cinestar Zadar"
+	},
+	{
+		id: 6, name: "Cinestar Slavonski Brod"
+	},
+	{
+		id: 7, name: "Cinestar Pula"
+	}
+]
--- js/components/home/FilterSection.js
+++ js/components/home/FilterSection.js
@@ -1,21 +1,29 @@
 const FilterSection = {
     template: /*html*/`
     <div class="filter-section">
-        <select
-          id="select-city"
-        >
-          <option default hidden>
-            Odaberi kino
-          </option>
+        <select id="select-city" v-model="selectedId">
+            <option disabled value="">Odaberi kino</option>
+            <option v-for="city in cities" :value="city.id" :key="city.id">{{city.name}}</option>
         </select>
         <div class="input-search">
-          <input
-            class="input"
-            type="text"
-            placeholder="Traži na Cinestaru"
-          />
+            <input
+                class="input"
+                type="text"
+                placeholder="Traži na Cinestaru"
+            />
           <i class="fa fa-search"></i>
         </div>
     </div>
-    `
+    `,
+    data() {
+        return {
+            selectedId: '',
+            cities: dataCity
+        }
+    },
+    watch: {
+        selectedId() {
+            console.log('selected id of record->', this.selectedId)
+        }
+    }
 }
