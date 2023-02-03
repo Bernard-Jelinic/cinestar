8fc35895ab0c1bca96818fb26391df8855c31900
Bernard-Jelinic <jelinic.bernard@gmail.com>
2023-02-03 20:59:42 +0100

    Add component BoxZanrTime

---
+++ js/components/home/BoxZanrTime.js
@@ -0,0 +1,12 @@
+const BoxZanrTime = {
+    template: /*html*/`
+    <div class="box-zanr-time">
+        <div id="Odabir prema žanru" class="zanr">
+          Odabir prema žanru
+        </div>
+        <div id="Odabir prema vremenu prikazivanja" class="time">
+          Odabir prema vremenu prikazivanja
+        </div>
+    </div>
+    `
+}
--- js/pages/home.js
+++ js/pages/home.js
@@ -2,13 +2,15 @@ const Home = {
     components: {
         'ImageContainer': ImageContainer,
         'FilterSection': FilterSection,
-        'CinestarName': CinestarName
+        'CinestarName': CinestarName,
+        'BoxZanrTime': BoxZanrTime
     },
     template: /*html*/`
     <div class="main-container">
         <ImageContainer/>
         <FilterSection/>
         <CinestarName/>
+        <BoxZanrTime/>
     </div>
     `
 }
