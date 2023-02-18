5ddbbe267a16567bd84eda9b60b3bd43bdab46c3
Bernard-Jelinic <jelinic.bernard@gmail.com>
2023-02-18 08:49:57 +0100

    Display genres and times in displayzanrtime component

--- js/components/home/DisplayZanrTime.js
+++ js/components/home/DisplayZanrTime.js
@@ -1,11 +1,18 @@
 const DisplayZanrTime = {
-    template: `
+    template: /*html*/`
     <div class="display-zanr-time">
         <ul>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
+            <li v-for="genre in genres">{{genre}}</li>
+        </ul>
+        <ul>
+            <li v-for="time in times">{{time}}</li>
         </ul>
     </div>
-    `
+    `,
+    data() {
+        return {
+            genres: ["Komedija", "Drama", "Horor", "Sf", "Akcija", "Kriminalistički", "Drama"],
+            times: ["Ponedjeljak", "Utorak", "Srijeda", "Četvrtak", "Petak", "Subota", "Nedjelja"]
+        }
+    }
 }
